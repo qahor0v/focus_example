5f7c75c392f7f0ce8551beadc1596a0721a14300
qahor0v <dalerovbekzod@gmail.com>
2023-05-30 17:51:16 +0500

    first commit

--- mysample/lib/main.dart
+++ mysample/lib/main.dart
@@ -7,6 +7,7 @@ class FocusTraversalGroupExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: FocusTraversalGroupExample(),
     );
   }
